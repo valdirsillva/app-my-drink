8389b953dd98bcef02251883389becb1bbee9e9a
Valdir Silva <valdipiresba@gmail.com>
2023-08-23 21:27:40 -0300

    Criando setup do projeto

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const PORT = 8080;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send({ name: "Jhon Doe" })
+})
+
+app.listen(PORT, () => {
+    console.log("APP RUNNING: http://127.0.0.1:", + PORT)
+});
